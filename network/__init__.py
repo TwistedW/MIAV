62204457d8fb6efdafcd4a2f9fc30acf860680cc
TwistedW <2359375192@qq.com>
2023-12-08 15:25:01 +0800

    Delete network directory

--- network/__init__.py
+++
@@ -1,8 +0,0 @@
-# from .LightMINet import *
-# from .MRNet import MRNet_VGG16_AV, MRNet_Res50_AV, MRNet_Res34_AV, MRNet_Res18_AV, MRNet_VGG19_TR, VGG_19
-from .Discriminator import Discriminator_E2E_AV, Discriminator
-# from .PCNet import PCNet
-from .PCNet_ISE import PCNet_ISE
-
-# from .WSGNLightMINet import *
-# from .PureWSGNLightMINet import *
